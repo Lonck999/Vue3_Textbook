337941adca3be6ea236d5cc9f0d8b30f578e8b13
Lonck999 <joe22053814@gmail.com>
2024-05-20 14:47:31 +0800

    update ref

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,9 @@
     <div>
       <VOn />
     </div>
+    <div>
+      <Ref />
+    </div>
   </div>
 </template>
 
@@ -28,6 +31,7 @@ import VIf from "./components/03_v-if_v-else-if_v-else/index.vue";
 import VShow from "./components/04_v-show/index.vue";
 import VFor from "./components/05_v-for/index.vue";
 import VOn from "./components/06_v-on/index.vue";
+import Ref from "./components/07_ref/index.vue";
 </script>
 
 <style scoped>
---
+++ src/components/07_ref/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>ref()</h1>
+    <div>現在總數：{{ state.count }}</div>
+    <button @click="add">+1</button>
+    <div>
+      <input v-model="inputRef" />
+      <button @click="focusInput">Focus Input</button>
+    </div>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+
+const state = ref({
+  count: 0,
+  message: "Hello",
+});
+const inputRef = ref("222");
+
+const add = () => {
+  state.value.count++;
+};
+
+function focusInput() {
+  inputRef.value += "!";
+  console.log(inputRef.value);
+}
+</script>
